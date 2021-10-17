30797b3d7f10451e513dcbccbe248c519c4917c5
Alena Zavadskaya <alena1yakusheva@gmail.com>
2021-10-17 10:55:37 +0300

    remove extra

--- scripts/index.js
+++ scripts/index.js
@@ -113,16 +113,3 @@ function toggleSubscriptionMonth() {
 
 annuallyMarkup.addEventListener("touchstart", toggleSubscriptionYear);
 monthlyMarup.addEventListener("touchstart", toggleSubscriptionMonth);
-
-/* iOS re-orientation fix */
-if (
-  navigator.userAgent.match(/iPhone/i) ||
-  navigator.userAgent.match(/iPad/i)
-) {
-  /* iOS hides Safari address bar */
-  window.addEventListener("load", function () {
-    setTimeout(function () {
-      window.scrollTo(0, 1);
-    }, 1000);
-  });
-}
